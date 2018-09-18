58225931705fe39721c105e220a1d2a1098eac54
Riasets <alextel2207@gmail.com>
2018-09-18 13:02:21 +0300

    rcc*2

---
+++ src/containers/schedule/Timeline.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Timeline extends React.Component {
+    render() {
+        return (
+            <div>
+                Таймлайн
+            </div>
+        );
+    }
+}
+
+export default Timeline;
---
+++ src/containers/schedule/createEvent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class CreateEvent extends React.Component {
+    render() {
+        return (
+            <div>
+                Создание События
+            </div>
+        );
+    }
+}
+
+export default CreateEvent;
