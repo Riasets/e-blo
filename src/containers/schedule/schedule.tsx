dc491ccda1d207573cdd84dec9a4ed77dfea13bc
Riasets <alextel2207@gmail.com>
2018-09-19 12:28:18 +0300

    nothing

--- src/containers/schedule/schedule.tsx
+++ src/containers/schedule/schedule.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
+import Timeline from "./Timeline";
+import CreateEvent from "./createEvent";
 
 class Schedule extends React.Component {
     public render() {
         return (
             <div>
-                <h3> Расписание</h3>
+                <Timeline />
+                <CreateEvent/>
             </div>
         );
     }
+}
+
+function mapStateToProps(){
+
 }
 
 export default Schedule;
