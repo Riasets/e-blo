36e5e9dba426482526b32e4da7b28cb536b3fd27
Riasets <alextel2207@gmail.com>
2018-09-22 14:58:41 +0300

    nothing

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -7,6 +7,9 @@ class Timeline extends React.Component {
     public render() {
         return (
             <div className={'timeline-container'}>
+                <h2>Таймлайн</h2>
+                <Day />
+                <Day />
                 <Day />
             </div>
         );
